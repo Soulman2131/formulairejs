47cacc08796a853213300447ca4acc46ccac6b10
Souleyman <109850920+Soulman2131@users.noreply.github.com>
2023-11-27 11:55:25 +0000

    Update 14 files

--- dist/index.ac9dc4ba.js
+++ dist/index.ac9dc4ba.js
@@ -576,19 +576,72 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"aR1JP":[function(require,module,exports) {
 var _stylesCss = require("./styles.css");
+const form = document.querySelector("form");
 const inputs = document.querySelectorAll('input[type="text"], input[type="password"] ');
-// CHECK
-const pseudoCheck = (value)=>{
-    console.log(value);
+const pEmail = document.getElementById("p-bar");
+// CONTAINER servant de base pour la Errordisplay
+// const pseudoContainer = document.querySelector(".pseudo-container");
+// const errorPseudo = document.querySelector(".pseudo-container > span");
+//FONCTION ERRORDISPLAY pseudo email password confirm pepc
+const errorDisplay = (pepc, message, valid)=>{
+    const pepcContainer = document.querySelector("." + pepc + "-container");
+    const pepcError = document.querySelector("." + pepc + "-container > span ");
+    if (!valid) {
+        pepcContainer.classList.add("error");
+        pepcError.textContent = message;
+    } else {
+        pepcContainer.classList.remove("error");
+        pepcError.textContent = message;
+    }
 };
+let pseudo, email, password, confirm;
+// CHECK
+// Pseudo
+const pseudoCheck = (value)=>{
+    if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+        errorDisplay("pseudo", "Le pseudo doit contenir au minimum 3, et au maximum 20 caract\xe9res ! ");
+        pseudo = null;
+    } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+        errorDisplay("pseudo", "Le pseudo ne doit pas contenir de caract\xe9res sp\xe9ciaux");
+        pseudo = null;
+    } else {
+        errorDisplay("pseudo", "", true);
+        pseudo = value;
+    }
+};
+// Email
 const emailCheck = (value)=>{
-    console.log(value);
+    if (!value.match(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/)) {
+        errorDisplay("email", "L'email n'est pas valide");
+        email = null;
+    } else {
+        errorDisplay("email", "", true);
+        email = value;
+    }
 };
 const passwordCheck = (value)=>{
-    console.log(value);
+    if (!value.match(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)) {
+        pEmail.classList.add("pRed");
+        errorDisplay("password", "Minimum de 8 caract\xe9res, un majuscule, un miniscule et un caract\xe9re sp\xe9cial", password = null);
+    } else if (value.length < 12) {
+        pEmail.classList.add("pBlue");
+        errorDisplay("password", "", true);
+        password = value;
+    } else {
+        pEmail.classList.add("pGreen");
+        errorDisplay("password", "", true);
+        password = value;
+    }
+    if (confirm) confirmCheck(confirm);
 };
 const confirmCheck = (value)=>{
-    console.log(value);
+    if (value != password) {
+        errorDisplay("confirm", "Les mots de passe ne correspondent pas ");
+        confirm = false;
+    } else {
+        errorDisplay("confirm", "", true);
+        confirm = true;
+    }
 };
 inputs.forEach((input)=>{
     input.addEventListener("input", (e)=>{
@@ -608,6 +661,20 @@ inputs.forEach((input)=>{
             default:
         }
     });
+});
+//ADD EVEN LISTENER
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    if (!pseudo || !email || !password || !confirm) alert("Veuillez remplir correctement les champs");
+    else {
+        alert(`Bravo ${pseudo} !! Votre inscription est valid\xe9e..`);
+        pseudo = "";
+        email = "";
+        password = "";
+        confirm = "";
+        inputs.forEach((input)=>input.value = "");
+        pEmail.classList = "";
+    }
 });
 
 },{"./styles.css":"lW6qc"}],"lW6qc":[function() {},{}]},["iZQ9B","aR1JP"], "aR1JP", "parcelRequirebaba")
--- src/index.mjs
+++ src/index.mjs
@@ -1,20 +1,94 @@
 import "./styles.css";
 
+const form = document.querySelector("form");
 const inputs = document.querySelectorAll(
   'input[type="text"], input[type="password"] ',
 );
-// CHECK
-const pseudoCheck = (value) => {
-  console.log(value);
+const pEmail = document.getElementById("p-bar");
+
+// CONTAINER servant de base pour la Errordisplay
+// const pseudoContainer = document.querySelector(".pseudo-container");
+// const errorPseudo = document.querySelector(".pseudo-container > span");
+
+//FONCTION ERRORDISPLAY pseudo email password confirm pepc
+const errorDisplay = (pepc, message, valid) => {
+  const pepcContainer = document.querySelector("." + pepc + "-container");
+  const pepcError = document.querySelector("." + pepc + "-container > span ");
+  if (!valid) {
+    pepcContainer.classList.add("error");
+    pepcError.textContent = message;
+  } else {
+    pepcContainer.classList.remove("error");
+    pepcError.textContent = message;
+  }
 };
+
+let pseudo, email, password, confirm;
+// CHECK
+// Pseudo
+const pseudoCheck = (value) => {
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay(
+      "pseudo",
+      "Le pseudo doit contenir au minimum 3, et au maximum 20 caractéres ! ",
+    );
+    pseudo = null;
+  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+    errorDisplay(
+      "pseudo",
+      "Le pseudo ne doit pas contenir de caractéres spéciaux",
+    );
+    pseudo = null;
+  } else {
+    errorDisplay("pseudo", "", true);
+    pseudo = value;
+  }
+};
+// Email
 const emailCheck = (value) => {
-  console.log(value);
+  if (
+    !value.match(
+      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+    )
+  ) {
+    errorDisplay("email", "L'email n'est pas valide");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
 };
 const passwordCheck = (value) => {
-  console.log(value);
+  if (
+    !value.match(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    )
+  ) {
+    pEmail.classList.add("pRed");
+    errorDisplay(
+      "password",
+      "Minimum de 8 caractéres, un majuscule, un miniscule et un caractére spécial",
+      (password = null),
+    );
+  } else if (value.length < 12) {
+    pEmail.classList.add("pBlue");
+    errorDisplay("password", "", true);
+    password = value;
+  } else {
+    pEmail.classList.add("pGreen");
+    errorDisplay("password", "", true);
+    password = value;
+  }
+  if (confirm) confirmCheck(confirm);
 };
 const confirmCheck = (value) => {
-  console.log(value);
+  if (value != password) {
+    errorDisplay("confirm", "Les mots de passe ne correspondent pas ");
+    confirm = false;
+  } else {
+    errorDisplay("confirm", "", true);
+    confirm = true;
+  }
 };
 
 inputs.forEach((input) => {
@@ -36,4 +110,20 @@ inputs.forEach((input) => {
         null;
     }
   });
+});
+
+//ADD EVEN LISTENER
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!pseudo || !email || !password || !confirm) {
+    alert("Veuillez remplir correctement les champs");
+  } else {
+    alert(`Bravo ${pseudo} !! Votre inscription est validée..`);
+    pseudo = "";
+    email = "";
+    password = "";
+    confirm = "";
+    inputs.forEach((input) => (input.value = ""));
+    pEmail.classList = "";
+  }
 });
